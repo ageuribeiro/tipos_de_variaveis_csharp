b823ec744621dcf4ddbf538ed24290a93b793634
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2023-10-10 18:04:45 -0300

    Experimentando os tipos de variaveis mais usados

--- tipos_de_variaveis_csharp/Program.cs
+++ tipos_de_variaveis_csharp/Program.cs
@@ -11,17 +11,38 @@ namespace tipos_de_variaveis_chsarp
     {
         static void Main(string[] args)
         {
-            // Tipagem estática
-            int y = 1;
-            //y = "s";
-
-
+           
             //Tipagem dinâmica
             var x = 1;
             //x = "sss";
 
+            // Tipagem estática
+            int y = 2;
+            //y = "s";
+
+            string s = "Ageu";
+
+            var s2 = s;
+
+            var f = 1.5f;
+            float f2 = 1.5f;
+
+            var d = 1.5;
+            double d2 = 1.5;
+
+            var bool1 = true;
+             
+            bool bool2 = false;
+
             Console.WriteLine(x);
             Console.WriteLine(y);
+            Console.WriteLine(s2);
+            Console.WriteLine(f);
+            Console.WriteLine(f2);
+            Console.WriteLine(d);
+            Console.WriteLine(d2);
+            Console.WriteLine(bool1);
+            Console.WriteLine(bool2);
         }
     }
 }
