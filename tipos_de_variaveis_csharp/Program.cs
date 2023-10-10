5a852794574df77026e34101b8357b5229534190
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2023-10-10 17:28:48 -0300

    Tipagem estática e tipagem dinâmica

--- tipos_de_variaveis_csharp/Program.cs
+++ tipos_de_variaveis_csharp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tipos_de_variaveis_chsarp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tipagem estática
+            int y = 1;
+            y = "s";
+
+
+            //Tipagem dinâmica
+            var x = 1;
+            x = "sss";
+        }
+    }
+}
